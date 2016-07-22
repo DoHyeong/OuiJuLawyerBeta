af1779e9076dbaaae6e030cdf9933e3dd6bbc74d
KIM <ambitiousdh@gmail.com>
2016-07-23 01:35:28 +0900

    .

--- app/src/main/java/ouijulawyer/project/soma/ouijulawyerbeta/View/MainActivity.java
+++ app/src/main/java/ouijulawyer/project/soma/ouijulawyerbeta/View/MainActivity.java
@@ -142,12 +142,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-
-
-
-
-
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -192,11 +186,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
