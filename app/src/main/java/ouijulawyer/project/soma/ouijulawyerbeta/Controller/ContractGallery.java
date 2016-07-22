8fa0f75206a1cb85977f28083ab91d4170fad109
KIM <ambitiousdh@gmail.com>
2016-07-23 01:28:44 +0900

    메인 기본기능
    기본적으로  메인화면 리팩토링(+ 나의 계약서 미리보기 까지만)

---
+++ app/src/main/java/ouijulawyer/project/soma/ouijulawyerbeta/Controller/ContractGallery.java
@@ -0,0 +1,59 @@
+package ouijulawyer.project.soma.ouijulawyerbeta.Controller;
+
+import android.content.Context;
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Transformation;
+import android.widget.Gallery;
+
+/**
+ * Created by KIM on 2016. 7. 23..
+ */
+public class ContractGallery extends Gallery {
+
+
+    private static Camera mCamera;
+    public ContractGallery(Context context) {
+        this(context,null);
+    }
+
+    public ContractGallery(Context context, AttributeSet attrs){
+        this(context,attrs,0);
+    }
+
+    public ContractGallery(Context context, AttributeSet attrs, int defStyle){
+        super(context, attrs, defStyle);
+        mCamera = new Camera();
+        setSpacing(-40);
+    }
+
+
+    // Toast toast = new Toast(getContext(),"asdf",Toast.LENGTH_SHORT);
+
+    protected boolean getChildStaticTransformation(View child, Transformation t) {
+        final int mCenter = (getWidth() - getPaddingLeft() - getPaddingRight()) / 2 + getPaddingLeft();
+        final int childCenter = child.getLeft() + child.getWidth() / 2;
+        final int childWidth = child.getWidth();
+
+        t.clear();
+        t.setTransformationType(Transformation.TYPE_MATRIX);
+        float rate = Math.abs((float) (mCenter - childCenter) / childWidth);
+
+        mCamera.save();
+        final Matrix matrix = t.getMatrix();
+        float zoomAmount = (float) (rate * 200.0);
+        mCamera.translate(0.0f, 0.0f, zoomAmount);
+        mCamera.getMatrix(matrix);
+        matrix.preTranslate(-(childWidth / 2), -(childWidth / 2));
+        matrix.postTranslate((childWidth / 2), (childWidth / 2));
+        mCamera.restore();
+
+        Log.d("trans","now");
+
+        return true;
+    }
+
+}
--- app/src/main/java/ouijulawyer/project/soma/ouijulawyerbeta/View/LoginActivity.java
+++ app/src/main/java/ouijulawyer/project/soma/ouijulawyerbeta/View/LoginActivity.java
@@ -102,9 +102,9 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this,"로그인실패",Toast.LENGTH_SHORT).show();
                 }else{
                     loading.dismiss();
-                   // Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                   // startActivity(intent);
-                   // finish();
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
 
 
